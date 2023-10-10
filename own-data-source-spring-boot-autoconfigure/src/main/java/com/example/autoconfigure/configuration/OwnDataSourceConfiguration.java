9e2c3a8a45da55a3bfcb4d2581dd4e640af66e0c
Vasilisa Marachkouskaya <vasilisa_marachkouskaya@epam.com>
2023-10-10 16:07:23 +0200

    refactor

--- own-data-source-spring-boot-autoconfigure/src/main/java/com/example/autoconfigure/configuration/OwnDataSourceConfiguration.java
+++ own-data-source-spring-boot-autoconfigure/src/main/java/com/example/autoconfigure/configuration/OwnDataSourceConfiguration.java
@@ -1,15 +1,10 @@
 package com.example.autoconfigure.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnResource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.Ordered;
 
 import javax.sql.DataSource;
 
